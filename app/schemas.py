79e8e80409e060be084ca56080f7038ce6fb4436
ShakingDonut <kacper.siembab@gmail.com>
2024-05-31 18:55:56 +0000

    add User, UserInDB, Token and TokenData classes

--- app/schemas.py
+++ app/schemas.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 
 from datetime import datetime
 
+
 class Book(BaseModel):
     id: int
     title: str
@@ -13,9 +14,36 @@ class Book(BaseModel):
     date_borrowed: datetime
     borrowed_by: str
 
+
 class BookAdd(BaseModel):
     title: str
     author: str
     year: int
     isbn: str
     branch: str
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+    name: str
+    surname: str
+    is_admin: bool = False
+    is_disabled: bool = False
+    date_created: datetime
+    date_updated: datetime
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    user_id: int
+    is_admin: bool
+
+
+class UserInDB(User):
+    hashed_password: str
